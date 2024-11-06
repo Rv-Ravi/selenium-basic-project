4376b0b183c513d93039f29aa9e933aa3c583509
Ravi <ravi@gmail.com>
2024-11-06 14:30:52 +0530

    test method to validate the cart page

--- sauce_demo/src/test/java/com/learning/selenium/ProductUnitTest.java
+++ sauce_demo/src/test/java/com/learning/selenium/ProductUnitTest.java
@@ -1,5 +1,7 @@
 package com.learning.selenium;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -9,6 +11,7 @@ import org.testng.annotations.Test;
 
 import com.learning.selenium.Pages.DashBoard;
 import com.learning.selenium.Pages.LoginPage;
+import com.learning.selenium.Pages.MyCartPage;
 import com.learning.selenium.Pages.ProductPage;
 import com.learning.selenium.Pages.ProductsPage;
 import com.learning.selenium.Utils.DriverFactory;
@@ -20,6 +23,7 @@ public class ProductUnitTest {
     ProductsPage itemsPage = null;
     ProductPage itemPage = null;
     DashBoard dashBoard = null;
+    MyCartPage cartPage = null;
 
     private static final String SITE_URL = "https://www.saucedemo.com/";
 
@@ -32,6 +36,7 @@ public class ProductUnitTest {
         dashBoard = new DashBoard(driver);
         itemsPage = new ProductsPage(driver);
         itemPage = new ProductPage(driver);
+        cartPage = new MyCartPage(driver);
     }
 
 
@@ -74,20 +79,61 @@ public class ProductUnitTest {
         Assert.assertTrue(dashBoard.isLogOutSuccess());
     }
 
+    @Test(dataProvider = "loginCreds")
+    public void validateCartPage(String username, String password, String[] details)
+    {
+        driver.get(SITE_URL);
+        
+        loginPage.loginTask(username, password);
+
+        Assert.assertTrue(loginPage.isLoginSuccessfull());
+
+        float priceSum = 0.0f;
+
+        for(String item : details)
+        {
+            itemsPage.goToProductPage(item);
+            itemPage.addToCart();
+            Assert.assertTrue(itemPage.isIteamAddedToCart());
+            priceSum += itemPage.getItemPrice();
+            itemPage.allItemsPage();
+        }
+
+        Assert.assertEquals(Math.round(priceSum * 100.0f) / 100.0f, 95.97f);
+        dashBoard.goToCart();
+
+        for(String item : details)
+        {
+            Assert.assertNotNull(cartPage.checkItemInCart(item));
+        }
+
+        cartPage.continueShopping();
+        dashBoard.logOutAction();
+        Assert.assertTrue(dashBoard.isLogOutSuccess());
+    }
+
     @DataProvider(name="loginCreds")
     public Object[][] loginCreds()
     {
 
 
         return new Object[][] {{"standard_user","secret_sauce","Sauce Labs Bolt T-Shirt","Sauce Labs Backpack",
-                                "Sauce Labs Fleece Jacket"},
-                            {"problem_user","secret_sauce","Sauce Labs Bolt T-Shirt","Sauce Labs Backpack",
                                 "Sauce Labs Fleece Jacket"}};
 
         
 
     }
 
+    private void delay(int seconds)
+    {
+        try{
+            Thread.sleep(Duration.ofSeconds(seconds));
+        }
+        catch(Exception E)
+        {
+            E.printStackTrace();
+        }
+    }
 
 
 
